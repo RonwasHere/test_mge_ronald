1998ba5eed7d018a491ccac85d1d5971482069d8
RonwasHere <ronaldliansugihartono@gmail.com>
2023-05-02 14:09:44 +0700

    muncul bagian angka

--- lib/pages/input.dart
+++ lib/pages/input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Input extends StatefulWidget {
   const Input({Key? key}) : super(key: key);
@@ -8,14 +9,25 @@ class Input extends StatefulWidget {
 }
 
 class _InputState extends State<Input> {
+  //untuk menangkap hasil inputan textformfield
   final TextEditingController nameController = TextEditingController();
-
+  final TextEditingController angkaController = TextEditingController();
+  //method untuk bagian kesatu
   void masuk() {
     setState(() {
       nameController.text;
       // nameController.text.replaceAll("a", "o");
     });
   }
+
+  //method untuk bagian kedua
+  void keluar() {
+    setState(() {
+      angkaController.text;
+    });
+  }
+
+  //method untuk desimal 3 angka di depan coma
 
   @override
   Widget build(BuildContext context) {
@@ -93,6 +105,69 @@ class _InputState extends State<Input> {
               padding: const EdgeInsets.all(8.0),
               child: Text(
                   'jumlah huruf : ' + nameController.text.length.toString()),
+            ),
+            //========================= bagian 2======================
+            //========================= bagian 2======================
+            //========================= bagian 2======================
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 200,
+                  child: TextFormField(
+                    controller: angkaController,
+                    decoration: InputDecoration(
+                      labelText: "Input Angka",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Container(
+                  width: 200,
+                  height: 50,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        side: BorderSide(color: Colors.blue),
+                      ),
+                    ),
+                    onPressed: () {
+                      keluar();
+                    },
+                    child: Text(
+                      'Run Angka',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              //print sesuai inputan
+              child: Text('Angka Asli : ' + angkaController.text),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              //print inputan, tapi 3 angka dibelakang
+              child: Text('Format : ' +
+                  NumberFormat.currency(locale: "id_ID")
+                      .format(123456)
+                      .toString()),
+            ),
+
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Angka Pangkat : ' + nameController.text.toLowerCase(),
+              ),
             ),
           ],
         ),
